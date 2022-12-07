7bead12c0239dbae05a628f29f7b6d98584215a9
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-07 16:34:08 -0500

    fixing src-srw

--- client/src-sw.js
+++ client/src-sw.js
@@ -35,10 +35,6 @@ registerRoute(
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
     ],
   })
 );
